7932cc44a9676e8505e0230c4aa49bd7a461c9ec
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-07-25 17:06:48 +0300

    React_Static-List-Of-Todos-JS fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
 import { TodoList } from './components/TodoList/TodoList';
 
-function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
-}
+export const App = () => {
+  function getUserById(userId) {
+    return usersFromServer.find(user => user.id === userId) || null;
+  }
 
-export const todos = todosFromServer.map(todo => ({
-  ...todo,
-  user: getUserById(todo.userId),
-}));
+  const todos = todosFromServer.map(todo => ({
+    ...todo,
+    user: getUserById(todo.userId),
+  }));
 
-export const App = () => (
-  <div className="App">
-    <h1 className="App__title">Static list of todos</h1>
-    <TodoList todos={todos} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1 className="App__title">Static list of todos</h1>
+      <TodoList todos={todos} />
+    </div>
+  );
+};
